f14dd8047f7dcced91d28770e803956bd784847c
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-04-08 00:29:57 -0400

    Data stored locally

---
+++ src/data.js
@@ -0,0 +1,84 @@
+var DATA = [
+    {
+       "name": "crash reporttttt",
+       "link":"chrome://crashes",
+       "description":"allows you to see chrome's crash report"
+    },
+    {
+       "name":"history",
+       "link":"chrome://history",
+       "description":"allows you to look at your chrome history"
+    },
+    {
+       "name":"top sites",
+       "link":"chrome://thumbnails",
+       "description":"allows you to see your most visited sites"
+    },
+    {
+       "name":"gpu",
+       "link":"chrome://gpu",
+       "description":"view all the gpu stats related to chrome"
+    },
+    {
+       "name":"new tab",
+       "link":"chrome://newtab",
+       "description":"allows you to open a new chrome tab"
+    },
+    {
+       "name":"user actions",
+       "link":"chrome://user-actions",
+       "description":"allows you to see your actions while tab is active"
+    },
+    {
+       "name":"system",
+       "link":"chrome://system",
+       "description":"allows you to view your system diagnostic data"
+    },
+    {
+       "name":"apps",
+       "link":"chrome://apps",
+       "description":"allows you to view and access all of your chrome applications"
+    },
+    {
+       "name":"downloads",
+       "link":"chrome://downloads",
+       "description":"allows you to view all web downloads made from chrome"
+    },
+    {
+       "name":"extensions",
+       "link":"chrome://extensions",
+       "description":"allows you to see what chrome extensions are running"
+    },
+    {
+       "name":"version",
+       "link":"chrome://version",
+       "description":"shows your chrome stats"
+    },
+    {
+       "name":"plugins",
+       "link":"chrome://plugins",
+       "description":"allows you to see what chrome plugins are running"
+    },
+    {
+       "name":"settings",
+       "link":"chrome://settings",
+       "description":"allows you to access the chrome settings menu"
+    },
+    {
+       "name":"bookmarks",
+       "link":"chrome://bookmarks",
+       "description":"allows you to open, view, and edit chrome bookmarks"
+    },
+    {
+       "name":"flags",
+       "link":"chrome://flags",
+       "description":"allows you to try chrome's experimental features"
+    },
+    {
+       "name":"restart",
+       "link":"chrome://restart",
+       "description":"allows you to restart the chrome application"
+    }
+ ];
+
+ export default DATA;
